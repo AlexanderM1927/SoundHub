4099a5d9c00f5e7191ade75d53726f7db46a4b0f
DavidJoan53 <davidjoan53@gmail.com>
2021-03-28 19:45:17 -0500

    navbar-user

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,36 +1,87 @@
 <template>
-    <q-header class="bg-grey" elevated>
-        <q-toolbar class="row justify-around">
-            <div class="col-3">
-                <q-toolbar-title>
-                SoundHub
-                </q-toolbar-title>
-            </div>
+  <div>
+    <q-header elevated class="bg-grey">
+        <q-toolbar>
+          <q-btn flat @click="side_options = !side_options" round dense icon="menu" />
+          <q-toolbar-title>SoundHub</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
 
-            <div class="col-6">
-                <q-input dark dense borderless v-model="search_content" class="q-ml-md search_input">
+      <q-drawer
+        v-model="side_options"
+        show-if-above
+        :width="300"
+        :breakpoint="500"
+        bordered
+        content-class="bg-grey"
+      >
+        <q-scroll-area class="fit">
+          <q-list>
+            <!--SEARCH BAR-->
+            <q-item>
+              <q-input dark dense borderless v-model="search_content" class="q-ml-xs search_input full-width" placeholder="Buscar...">
                 <template v-slot:prepend>
                     <q-icon v-if="search_content === ''" name="fas fa-search" />
                     <q-icon v-else name="fas fa-times" class="cursor-pointer" @click="search_content = ''" />
                 </template>
-                </q-input>
-            </div>
-
-            <div class="row col-3 justify-end">
-                <q-btn flat class="no-ripple" @click="logout">
-                    <q-icon name="fas fa-sign-out-alt"></q-icon>
-                </q-btn>
-            </div>
-        </q-toolbar>
-    </q-header>
+              </q-input>
+            </q-item>
+            <!--ITEMS-->
+            <template v-for="(menuItem, index) in menuList">
+              <q-item class="light_font" :key="index" clickable :active="menuItem.label === 'Outbox'" v-ripple>
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon"/>
+                </q-item-section>
+                <q-item-section >
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
+            </template>
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+    </div>
 </template>
 
 <script>
+import { functions } from '../functions.js'
+const menuList = [
+  {
+    icon: 'far fa-user-circle',
+    label: 'Perfil',
+    separator: true
+  },
+  {
+    icon: 'fas fa-heart',
+    label: 'Favoritos',
+    separator: false
+  },
+  {
+    icon: 'fas fa-play',
+    label: 'Mis listas',
+    separator: false
+  },
+  {
+    icon: 'fas fa-bell',
+    label: 'Notificaciones',
+    separator: true
+  },
+  {
+    icon: 'fas fa-sign-out-alt',
+    iconColor: 'primary',
+    label: 'Salir',
+    separator: false
+  }
+]
 export default {
   name: 'Navbar',
+  mixins: [functions],
   data () {
     return {
-      search_content: ''
+      search_content: '',
+      side_options: true,
+      menuList
     }
   },
   methods: {
@@ -43,9 +94,14 @@ export default {
 
 <style>
 .search_input{
-    padding-left: 10px;
-    background-color: #505057;
-    font-size: 20px;
-    border-radius: 5px;
+  padding-left: 10px;
+  background-color: #505057;
+  font-size: 18px;
+  border-radius: 5px;
+}
+
+.light_font {
+  color: #f5f5f5;
+  font-size: 16px;
 }
 </style>
--- front/src/layouts/LoginLayout.vue
+++ front/src/layouts/LoginLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh Lpr lff">
     <q-page-container>
       <router-view />
     </q-page-container>
