696905804296b6142569f9a1f872504f5cac4208
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2021-04-08 14:21:05 -0500

    Create soundController.js

---
+++ back/v1/controllers/soundController.js
@@ -0,0 +1,15 @@
+'use strict'
+const Sound = require('../models').sound
+const Joi = require('@hapi/joi');
+
+exports.show = async function(req, res) {
+  
+}
+
+exports.store = async function(req, res) {
+  
+}
+
+exports.update = async function(req, res) {
+
+}
