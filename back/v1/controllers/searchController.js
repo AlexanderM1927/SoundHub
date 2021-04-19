c48d8a8e494b40b9b86597e3b06d225dc48427d1
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2021-04-18 21:53:51 -0500

    exclude videos with duration upper than 1 hour

--- back/v1/controllers/searchController.js
+++ back/v1/controllers/searchController.js
@@ -32,7 +32,10 @@ exports.search = async function(req, res) {
     for (let i = 0; i < youtube.items.length; i++) {
       const video = youtube.items[i]
       // console.log(video.length.accessibility.accessibilityData)
-      if (video.type === 'video' && video.length.accessibility) results.items.push(video)
+      if (video.type === 'video' && video.length.accessibility && video.length.simpleText.match(/:/g).length === 1) {
+        // console.log(Moment(video.length.accessibility.accessibilityData))
+        results.items.push(video)
+      }
     }
     results.nextPage = youtube.nextPage
     res.json({
