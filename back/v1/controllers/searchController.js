1912f6a607e2d0cdbe5970480609cfd57fe93ea3
John A. Martinez <jhon71937@gmail.com>
2023-06-08 17:42:23 -0500

    Fix

--- back/v1/controllers/searchController.js
+++ back/v1/controllers/searchController.js
@@ -53,13 +53,12 @@ exports.download = async function(req, res) {
   try {
     const url = req.params.url;
     const type = req.params.type;
-    res.setHeader("Content-Type", "audio/mpeg");
+    res.setHeader("Content-Type", "audio/mp4");
     res.setHeader('Content-disposition', 'attachment; filename=' + Date.now() + '.m4a');
     res.setHeader("Access-Control-Allow-Origin", "*");
     if (type === 'video') {
       ytdl(url, {
         filter: 'audioonly',
-        quality: 'audioonly',
         format: 'm4a'
       }).pipe(res)
     } else {
