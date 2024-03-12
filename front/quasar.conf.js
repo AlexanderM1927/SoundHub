a588e48cc90adf24eda2af89e7802c13b6dfff4b
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-03-11 21:29:12 -0500

    Dockerize project

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -7,7 +7,6 @@
 // https://quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-env node */
 const DotEnv = require('dotenv')
-const Webpack = require('Webpack')
 // get out env variables
 const envparsers = require('./envparser')
 const envparser = require('./envparser')
@@ -82,7 +81,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 9093,
       // open: true // opens browser window automatically
     },
 
