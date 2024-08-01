4e889da81e38011d910d491255e5c99dfd7e87df
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-08-01 12:14:06 -0500

    Deleted newfrontend folder

--- newfrontend/src/services/ViewService.js
+++
@@ -1,7 +0,0 @@
-import Api from '../boot/axios'
-
-export default {
-  getCommentsBySound () {
-    return Api().get('views')
-  }
-}
--- newfrontend/src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-
-export type Sound = {
-    url: string
-} | null
