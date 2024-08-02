c2f0efff29141c6e1c42c7faf3f71d71133be801
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-08-01 23:40:48 -0500

    test json

--- back/app.ts
+++ back/app.ts
@@ -2,10 +2,10 @@ import express, { json } from 'express' // require -> commonJS
 import { corsMiddleware } from './middlewares/cors'
 import { createRouter } from './routes/index'
 import cookieParser from 'cookie-parser'
-import path from 'path'
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
 
 export const createApp = ({ acceptedOrigins }: { acceptedOrigins: any }) => {
