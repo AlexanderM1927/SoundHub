aac68cb7a053c149f34caa5e891e3e7bef40b549
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-08-13 19:27:48 -0500

    Delete all emojis

--- front/src/functions.js
+++ front/src/functions.js
@@ -230,7 +230,8 @@ export const functions = {
       // clean speciall words and []
       result = title.replace(/(\(|\)|\[|\]|official|oficial|video|lyric|lyrics|letra)/gi, '')
       // clean emojis and []
-      result = result.replace(/[\[\]\(\)\u{1F600}-\u{1F64F}\u{2700}-\u{27BF}]+/gu, '').trim()
+      result = result.replace(/[\[\]\(\)\u{1F600}-\u{1F64F}\u{2700}-\u{27BF}]+/gu, '')
+      result = result.replace(/[\[\]\(\)\p{Emoji_Presentation}\p{Extended_Pictographic}]+/gu, '').trim()
       return result
     }
   }
